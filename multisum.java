e28cf8d9c2006c4b22b7f625a826d96f2585b6ff
Rudranil Goswami (Rudy) <151756615+Code-With-Rudy@users.noreply.github.com>
2025-07-19 02:10:47 +0530

    Create multisum.java

---
+++ multisum.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Arithmetic {
+
+    public Integer sum(Integer[] ints) {
+        int total = 0;
+        for (Integer num : ints) {
+            total += num;
+        }
+        return total;
+    }
+
+    public String sum(String[] strs) {
+        StringBuilder result = new StringBuilder();
+        for (String str : strs) {
+            result.append(str);
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Arithmetic a = new Arithmetic();
+
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine().trim();
+            if (line.isEmpty()) continue;
+
+            String[] tokens = line.split(" ");
+
+            boolean isInteger = true;
+            for (String token : tokens) {
+                if (!token.matches("\\d+")) {
+                    isInteger = false;
+                    break;
+                }
+            }
+
+            if (isInteger) {
+                Integer[] nums = new Integer[tokens.length];
+                for (int i = 0; i < tokens.length; i++) {
+                    nums[i] = Integer.parseInt(tokens[i]);
+                }
+                System.out.println(a.sum(nums));
+            } else {
+                System.out.println(a.sum(tokens));
+            }
+        }
+
+        sc.close();
+    }
+}
