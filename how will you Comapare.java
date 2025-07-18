40406c634d1acf1bf1cb86e3bad4952d96d4ad0a
Rudranil Goswami (Rudy) <151756615+Code-With-Rudy@users.noreply.github.com>
2025-07-19 02:14:53 +0530

    Create how will you Comapare.java

---
+++ how will you Comapare.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class Arithmetic {
+
+    // Sum integers
+    public Integer sum(Integer[] ints) {
+        int total = 0;
+        for (Integer num : ints) {
+            total += num;
+        }
+        return total;
+    }
+
+    // Concatenate strings
+    public String sum(String[] strs) {
+        StringBuilder result = new StringBuilder();
+        for (String str : strs) {
+            result.append(str);
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Arithmetic a = new Arithmetic();
+
+        // Read number of test cases
+        int T = Integer.parseInt(sc.nextLine());
+
+        for (int t = 0; t < T; t++) {
+            String line = sc.nextLine().trim();
+            String[] tokens = line.split(" ");
+
+            // Check if it's integers or strings
+            boolean isInteger = true;
+            for (String token : tokens) {
+                if (!token.matches("\\d+")) {
+                    isInteger = false;
+                    break;
+                }
+            }
+
+            if (isInteger) {
+                Integer[] nums = new Integer[tokens.length];
+                for (int i = 0; i < tokens.length; i++) {
+                    nums[i] = Integer.parseInt(tokens[i]);
+                }
+                System.out.println(a.sum(nums));
+            } else {
+                System.out.println(a.sum(tokens));
+            }
+        }
+
+        sc.close();
+    }
+}
